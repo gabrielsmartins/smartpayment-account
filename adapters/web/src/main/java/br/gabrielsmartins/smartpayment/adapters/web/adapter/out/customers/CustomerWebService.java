d5440295332b1f9d0c0455df41d06594fd1676da
Gabriel Martins <ga.smartins94@gmail.com>
2020-08-02 21:18:59 -0300

    Adicionando Dockerfile

--- adapters/web/src/main/java/br/gabrielsmartins/smartpayment/adapters/web/adapter/out/customers/CustomerWebService.java
+++ adapters/web/src/main/java/br/gabrielsmartins/smartpayment/adapters/web/adapter/out/customers/CustomerWebService.java
@@ -6,7 +6,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient
+@FeignClient(value = "${customers.endpoint}")
 public interface CustomerWebService {
 
     @GetMapping("/{id}")
